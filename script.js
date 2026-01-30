03250edbe138bfa7e40575ed952174419c6237c7
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-01-29 20:06:34 -0800

    start coding

--- script.js
+++ script.js
@@ -317,3 +317,11 @@ document.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('resize', () => {
     ScrollTrigger.refresh();
 });
+
+const arr = [1,2,3];
+
+arr.push(4)
+
+arr = [1,2,3,4]
+
+console.log(arr)
